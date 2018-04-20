053035385586698f263fcc451fc8beb99267c83d
roger yee <ryee.epicodus@gmail.com>
2018-04-20 15:41:05 -0700

    passes box validation tests

--- app/models/box.rb
+++ app/models/box.rb
@@ -1,3 +1,6 @@
 class Box < ActiveRecord::Base
   validates :name, :presence => true
+  validates :size, :presence => true
+  validates :color, :presence => true
+  validates :price, :presence => true
 end
--- spec/models/boxes_spec.rb
+++ spec/models/boxes_spec.rb
@@ -2,4 +2,7 @@ require 'rails_helper'
 
 describe Box do
   it { should validate_presence_of :name }
+  it { should validate_presence_of :size }
+  it { should validate_presence_of :color }
+  it { should validate_presence_of :price }
 end
