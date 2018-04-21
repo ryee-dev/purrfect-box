89828ca71d347720e8f4fc9760dfbf2d1a9df94b
roger yee <ryee.epicodus@gmail.com>
2018-04-20 19:46:47 -0700

    updates ruby version, updates gems, able to run server from home

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_20_233044) do
+ActiveRecord::Schema.define(version: 2018_04_20_232218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2018_04_20_233044) do
   create_table "reviews", force: :cascade do |t|
     t.integer "box_id"
     t.string "name"
-    t.string "review"
+    t.text "review"
     t.integer "rating"
   end
 
