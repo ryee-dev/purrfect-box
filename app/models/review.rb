fe8e35e9cbaf2c4fd3412f01d478358a2c9c3001
roger yee <ryee.epicodus@gmail.com>
2018-04-24 21:39:46 -0700

    adds validations for checking review length and rating interval

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,5 +4,7 @@ class Review < ActiveRecord::Base
   validates :box_id, :presence => true
   validates :name, :presence => true
   validates :review, :presence => true
+  validates :review, length: { minimum: 50, maximum: 250 }, allow_blank: false
   validates :rating, :presence => true
+  validates :rating, :inclusion => 1..5
 end
