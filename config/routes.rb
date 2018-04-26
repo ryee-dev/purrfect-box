7598a930d999435826f9793d19a2c873510cbf2b
roger yee <ryee.epicodus@gmail.com>
2018-04-25 20:44:02 -0700

    formats registration and sign-in/up/out links

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'boxes#index'
   get '/boxes', to: 'boxes#allBoxes'
+
   resources :boxes do
     resources :reviews, :except => [:index]
   end
