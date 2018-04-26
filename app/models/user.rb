3d126dc7e485353e5d7c9e9f01b44b0c4e756a1b
roger yee <ryee.epicodus@gmail.com>
2018-04-25 20:19:34 -0700

    adds username and other related functionalities

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,28 @@
 class User < ApplicationRecord
+  attr_accessor :login
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # Validations
+  validates :username, presence: :true, uniqueness: { case_sensitive: false }
+
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      if conditions[:username].nil?
+        where(conditions).first
+      else
+        where(username: conditions[:username]).first
+      end
+    end
+
+    conditions[:email].downcase! if conditions[:email]
+    where(conditions.to_h).first
+  end
+
 end
